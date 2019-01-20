86eb7b9c4a461dd03a88aea8e16fdbae180e9050
Tammy Metz <tammy@tammymetz.com>
2019-01-20 16:02:50 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with ClickbaitValidator
+  validates :title, clickbait: true
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Non-Fiction Fiction) }
 end
