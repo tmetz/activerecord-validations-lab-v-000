facdc38d6153ab57da82fe7a26f2073301985076
Tammy Metz <tammy@tammymetz.com>
2019-01-20 16:59:11 +0000

    Automatically backed up by Learn

--- app/models/concerns/clickbait_validator.rb
+++ app/models/concerns/clickbait_validator.rb
@@ -1,6 +1,6 @@
 class ClickbaitValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    phrase_array = ["Won't Believe", "Secret", "Top", "Guess"]
+    phrase_array = ["Won't Believe", "Secret", "Top /[0-9]/", "Guess"]
     if phrase_array.any? {|phrase| value.to_s.include?(phrase)}
       return true
     else
@@ -8,3 +8,17 @@ class ClickbaitValidator < ActiveModel::EachValidator
     end
   end
 end
+
+# solution from Flatiron:
+# CLICKBAIT_PATTERNS = [
+#     /Won't Believe/i,
+#     /Secret/i,
+#     /Top [0-9]*/i,
+#     /Guess/i
+#   ]
+# 
+#   def is_clickbait?
+#     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+#       errors.add(:title, "must be clickbait")
+#     end
+#   end
